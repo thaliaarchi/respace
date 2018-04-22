108925059f1aed486faf2de02bbb052aed86d673
Andrew Archibald <andrew@robarchibald.com>
2018-04-22 03:10:43 -0600

    Create Cursor for BitCursor and CharCursor to inherit from

--- wsbinary.h
+++ wsbinary.h
@@ -9,16 +9,29 @@ namespace WS {
     const size_t WS_BUFFER_CAPACITY = 512;
     const size_t BIN_BUFFER_CAPACITY = WS_BUFFER_CAPACITY / 4;
 
-    // Manages reading and writing of binary files bit by bit
-    class BitCursor {
+    class Cursor {
     protected:
         FILE* stream;
-        block_t buffer[BIN_BUFFER_CAPACITY];
+        block_t* buffer;
+        const size_t buffer_capacity;
         int buffer_i = 0;
-        int bit_i = HIGH_BIT;
-        static const int HIGH_BIT = 8 * sizeof(block_t) - 1;
 
-        BitCursor(FILE* stream) : stream(stream) {}
+    public:
+        Cursor(FILE* stream, size_t buffer_capacity) : stream(stream),
+            buffer_capacity(buffer_capacity), buffer(new block_t[buffer_capacity]) {}
+
+        ~Cursor() {
+            delete[] buffer;
+        }
+    };
+
+    // Manages reading and writing of binary files bit by bit
+    class BitCursor : public Cursor {
+    protected:
+        static const int HIGH_BIT = 8 * sizeof(block_t) - 1;
+        int bit_i = HIGH_BIT;
+
+        BitCursor(FILE* stream) : Cursor(stream, BIN_BUFFER_CAPACITY) {}
     };
 
     class BitReader : public BitCursor {
@@ -27,7 +40,7 @@ namespace WS {
 
     public:
         BitReader(FILE* stream) : BitCursor(stream) {
-            buffer_size = fread(buffer, sizeof(block_t), BIN_BUFFER_CAPACITY, stream);
+            buffer_size = fread(buffer, sizeof(block_t), buffer_capacity, stream);
         }
 
         block_t readBit() {
@@ -36,7 +49,7 @@ namespace WS {
             if (bit_i < 0) {
                 buffer_i++;
                 if (buffer_i >= buffer_size) {
-                    buffer_size = fread(buffer, sizeof(block_t), BIN_BUFFER_CAPACITY, stream);
+                    buffer_size = fread(buffer, sizeof(block_t), buffer_capacity, stream);
                     buffer_i = 0;
                 }
                 bit_i = HIGH_BIT;
@@ -64,8 +77,8 @@ namespace WS {
             if (bit_i < 0) {
                 buffer[buffer_i] = block;
                 buffer_i++;
-                if (buffer_i >= BIN_BUFFER_CAPACITY) {
-                    fwrite(buffer, sizeof(block_t), BIN_BUFFER_CAPACITY, stream);
+                if (buffer_i >= buffer_capacity) {
+                    fwrite(buffer, sizeof(block_t), buffer_capacity, stream);
                     buffer_i = 0;
                 }
                 block = 0;
