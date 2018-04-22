febf2e701a3732b72b25034695a96bf3b6e2590b
Andrew Archibald <andrew@robarchibald.com>
2018-04-22 12:23:31 -0600

    Merge CharReader and BitReader into Reader and CharWriter and BitWriter into Writer

--- wsbinary.h
+++ wsbinary.h
@@ -9,96 +9,53 @@ namespace WS {
     const size_t WS_BUFFER_CAPACITY = 512;
     const size_t BIN_BUFFER_CAPACITY = WS_BUFFER_CAPACITY / 4;
 
-    class Cursor {
+    class BufferIO {
     protected:
         FILE* stream;
         block_t* buffer;
         const size_t buffer_capacity;
+        size_t buffer_size = 0;
         int buffer_i = 0;
+        block_t bit_block = 0;
+        int bit_i = HIGH_BIT;
+        static const int HIGH_BIT = 8 * sizeof(block_t) - 1;
+
+        BufferIO(FILE* stream, size_t buffer_capacity) : stream(stream),
+            buffer(new block_t[buffer_capacity]), buffer_capacity(buffer_capacity) {}
 
     public:
-        Cursor(FILE* stream, size_t buffer_capacity) : stream(stream),
-            buffer_capacity(buffer_capacity), buffer(new block_t[buffer_capacity]) {}
 
-        ~Cursor() {
+        ~BufferIO() {
             delete[] buffer;
         }
     };
 
-    class CharReader : public Cursor {
+    class Reader : public BufferIO {
     private:
-        size_t buffer_size = 0;
-
-    public:
-        CharReader(FILE* stream) : Cursor(stream, WS_BUFFER_CAPACITY) {
-            buffer_size = fread(buffer, sizeof(char), buffer_capacity, stream);
+        size_t readBuffer() {
+            buffer_size = fread(buffer, sizeof(block_t), buffer_capacity, stream);
+            buffer_i = 0;
+            return buffer_size;
         }
 
-        char readChar() {
+    public:
+        Reader(FILE* stream, size_t capacity) : BufferIO(stream, capacity) {
+            readBuffer();
+        }
+
+        block_t readBlock() {
             if (buffer_i >= buffer_size) {
-                buffer_size = fread(buffer, sizeof(block_t), buffer_capacity, stream);
-                buffer_i = 0;
+                readBuffer();
             }
+            bit_i = HIGH_BIT;
             return buffer[buffer_i++];
         }
 
-        void close() {
-            fclose(stream);
-        }
-
-        bool canRead() {
-            return this->buffer_size > 0;
-        }
-    };
-
-    class CharWriter : public Cursor {
-    public:
-        CharWriter(FILE* stream) : Cursor(stream, WS_BUFFER_CAPACITY) {}
-
-        void writeChar(char c) {
-            if (buffer_i >= buffer_capacity) {
-                fwrite(buffer, sizeof(block_t), buffer_capacity, stream);
-                buffer_i = 0;
-            }
-            buffer[buffer_i++] = c;
-        }
-
-        void close() {
-            fwrite(buffer, sizeof(block_t), buffer_i, stream);
-            fclose(stream);
-        }
-    };
-
-    // Manages reading and writing of binary files bit by bit
-    class BitCursor : public Cursor {
-    protected:
-        static const int HIGH_BIT = 8 * sizeof(block_t) - 1;
-        int bit_i = HIGH_BIT;
-
-        BitCursor(FILE* stream) : Cursor(stream, BIN_BUFFER_CAPACITY) {}
-    };
-
-    class BitReader : public BitCursor {
-    private:
-        size_t buffer_size = 0;
-
-    public:
-        BitReader(FILE* stream) : BitCursor(stream) {
-            buffer_size = fread(buffer, sizeof(block_t), buffer_capacity, stream);
-        }
-
         block_t readBit() {
-            block_t bit = (buffer[buffer_i] >> bit_i) & 1;
-            bit_i--;
-            if (bit_i < 0) {
-                buffer_i++;
-                if (buffer_i >= buffer_size) {
-                    buffer_size = fread(buffer, sizeof(block_t), buffer_capacity, stream);
-                    buffer_i = 0;
-                }
-                bit_i = HIGH_BIT;
+            if (bit_i < 0) { // All bits in current block read
+                readBlock(); // Advance to next char
             }
-            return bit;
+            return (buffer[buffer_i] >> bit_i--) & 1;
         }
 
         void close() {
@@ -106,35 +63,47 @@ namespace WS {
         }
 
         bool canRead() {
-            return this->buffer_size > 0;
+            if (buffer_i >= buffer_size && buffer_size > 0) {
+                return readBuffer() > 0;
+            }
+            return buffer_size > 0;
         }
     };
 
-    class BitWriter : public BitCursor {
+    class Writer : public BufferIO {
     private:
-        block_t block = 0;
+        void writeBuffer(size_t size) {
+            fwrite(buffer, sizeof(block_t), size, stream);
+            buffer_i = 0;
+        }
 
     public:
-        BitWriter(FILE* stream) : BitCursor(stream) {}
+        Writer(FILE* stream, size_t capacity) : BufferIO(stream, capacity) {}
+
+        void writeBlock(block_t block) {
+            if (buffer_i >= buffer_capacity) {
+                writeBuffer(buffer_capacity);
+            }
+            bit_i = HIGH_BIT;
+            buffer[buffer_i++] = block;
+        }
 
         void writeBit(block_t bit) {
             if (bit_i < 0) {
-                buffer[buffer_i] = block;
-                buffer_i++;
-                if (buffer_i >= buffer_capacity) {
-                    fwrite(buffer, sizeof(block_t), buffer_capacity, stream);
-                    buffer_i = 0;
-                }
-                block = 0;
-                bit_i = HIGH_BIT;
+                writeBlock(bit_block);
+                bit_block = 0;
             }
-            block |= bit << bit_i--;
+            bit_block |= bit << bit_i--;
         }
 
         void close() {
-            buffer[buffer_i] = block;
-            fwrite(buffer, sizeof(block_t), buffer_i + 1, stream);
+            writeBuffer(buffer_i);
             fclose(stream);
+        }
+        
+        void bitClose() {
+            buffer[buffer_i++] = bit_block;
+            close();
         }
     };
 
@@ -142,40 +111,40 @@ namespace WS {
         if (!in || !out) {
             return;
         }
-        CharReader reader(in);
-        BitWriter writer(out);
+        Reader reader(in, WS_BUFFER_CAPACITY);
+        Writer writer(out, BIN_BUFFER_CAPACITY);
 
         while (reader.canRead()) {
-            switch (reader.readChar()) {
-            case '\t': writer.writeBit(1); // Insert 10
-            case ' ': writer.writeBit(0); break; // Insert 0
-            case '\n': writer.writeBit(1); writer.writeBit(1); // Insert 11
+            switch (reader.readBlock()) {
+            case '\t': writer.writeBit(1); // [TAB] is stored as 10
+            case ' ': writer.writeBit(0); break; // [SPACE] is stored as 0
+            case '\n': writer.writeBit(1); writer.writeBit(1); // [LF] is stored as 11
             // Otherwise, ignore character
             }
         }
 
         reader.close();
-        writer.close();
+        writer.bitClose();
     }
 
     void fromBinary(FILE* in, FILE* out) {
         if (!in || !out) {
             return;
         }
-        BitReader reader(in);
-        CharWriter writer(out);
+        Reader reader(in, BIN_BUFFER_CAPACITY);
+        Writer writer(out, WS_BUFFER_CAPACITY);
 
         while (reader.canRead()) {
             if (reader.readBit()) {
                 if (!reader.canRead())
                     break;
                 if (reader.readBit())
-                    writer.writeChar('\n'); // [LF] is 11
+                    writer.writeBlock('\n'); // [LF] is read as 11
                 else
-                    writer.writeChar('\t'); // [TAB] is 10
+                    writer.writeBlock('\t'); // [TAB] is read as 10
             }
             else {
-                writer.writeChar(' '); // [SPACE] is 0
+                writer.writeBlock(' '); // [SPACE] is read as 0
             }
         }
         
