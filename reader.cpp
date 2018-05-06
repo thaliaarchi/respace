38c765fb35b64c9381e712e440724d8cadee095e
Andrew Archibald <andrew@robarchibald.com>
2018-05-05 19:14:42 -0700

    Move readBuffer after reading to fix erroneous hasNext output

--- reader.cpp
+++ reader.cpp
@@ -6,24 +6,23 @@ namespace WS {
     }
 
     block_t Reader::next() {
+        block_t block = buffer_[buffer_i_++];
         if (buffer_i_ >= buffer_size_) {
             readBuffer();
         }
         bit_i_ = HIGH_BIT_;
-        return buffer_[buffer_i_++];
+        return block;
     }
 
     block_t Reader::nextBit() {
+        block_t bit = (buffer_[buffer_i_] >> bit_i_--) & 1;
         if (bit_i_ < 0) { // All bits in current block read
             next(); // Advance to next block
         }
-        return (buffer_[buffer_i_] >> bit_i_--) & 1;
+        return bit;
     }
 
     bool Reader::hasNext() {
-        if (buffer_i_ >= buffer_size_ && buffer_size_ > 0) {
-            readBuffer();
-        }
         return buffer_size_ > 0;
     }
 
