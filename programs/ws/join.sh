4b5bd067531ee21cb97c0c08224cfeb1cf283c37
Andrew Archibald <andrew@aarchibald.com>
2018-05-19 14:32:54 -0700

    Add support for ; as command separator

--- programs/ws/join.sh
+++ programs/ws/join.sh
@@ -3,6 +3,7 @@
 # Remaining arguments are wsa files to join
 SCRIPTDIR=$(dirname "$0")
 cat ${@:3} > tmp.c
-sed -E -i 's/#.+$//g' tmp.c
+sed -E -i 's/\s*#.+$//g' tmp.c
+sed -E -i 's/\s*;\s*/\n/g' tmp.c
 gcc -E -P -include $2 tmp.c > $1
 rm tmp.c
