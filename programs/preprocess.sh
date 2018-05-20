0ab010028192ebbd209b907e2857246ccb2b6cd8
Andrew Archibald <andrew@aarchibald.com>
2018-05-19 21:09:58 -0700

    Update preprocess script to work in multiple directories

--- programs/preprocess.sh
+++ programs/preprocess.sh
@@ -1,5 +1,5 @@
 # ./preprocess.sh <input> <output>
-for i in {$1,*.wsa}; do
+for i in {$1,$(find . -type f -name "*.wsa")}; do
     cp "$i" "$i.tmp.c"
     sed -E -i '
         s/\s*#.+$//g
@@ -8,5 +8,5 @@ for i in {$1,*.wsa}; do
         s/^#include\s*"(.+?)"/#include "\1.tmp.c"/g
     ' "$i.tmp.c"
 done
-gcc -E -P $1.tmp.c > $2
-rm *.tmp.c
+gcc -E -P "$1.tmp.c" > "$2"
+find . -type f -name '*.tmp.c' -exec rm {} +
